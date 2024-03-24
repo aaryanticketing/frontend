31385ba9ba650ba657993efceb7b646c490673af
unknown <cselvitigala@gmail.com>
2024-03-23 19:51:15 -0500

    add

--- src/layout/OutletContainer.tsx
+++ src/layout/OutletContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MovieCard from "../components/movie/movieCard";
+import styles from "./Layout.module.css";
 
 function OutletContainer({
 }: {
@@ -8,8 +9,26 @@ function OutletContainer({
     isLoading?: boolean;
     breadcrumbs?: { label: string; link: string }[];
 }) {
+    const movies = [
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q1' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q5' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q6' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q7' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q8' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q9' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q10' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q11' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q112' },
+        { imgUrl: 'https://via.placeholder.com/150', title: 'q123' },
+        // Add more movies here
+      ];
     return (
-        <MovieCard imgUrl="https://via.placeholder.com/150" title="Movie 1" />
+        <div className={styles.container}>
+            {movies.map((movie, _) => (
+            <MovieCard imgUrl={movie.imgUrl} title={movie.title} />
+    ))}
+        </div>
     );
 }
 
